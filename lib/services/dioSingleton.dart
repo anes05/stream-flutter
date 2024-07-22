6da7a87a09537eebcd3a262113ab42eae92511fc
anes05 <anesmsaddak5@gmail.com>
2024-07-22 16:17:07 +0100

    added dio pretty logger

--- lib/services/dioSingleton.dart
+++ lib/services/dioSingleton.dart
@@ -1,4 +1,6 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class DioSingleton {
@@ -20,6 +22,25 @@ class DioSingleton {
         return handler.next(options);
       }
     ));
+    _dioInstance.interceptors.add(PrettyDioLogger(
+        requestHeader: true,
+        requestBody: true,
+        responseBody: true,
+        responseHeader: false,
+        error: true,
+        compact: true,
+        maxWidth: 90,
+        enabled: kDebugMode,
+        filter: (options, args){
+          // don't print requests with uris containing '/posts'
+        /*  if(options.path.contains('/posts')){
+            return false;
+          }*/
+          // don't print responses with unit8 list data
+          return !args.isResponse || !args.hasUint8ListData;
+        }
+    )
+    );
 
   }
 
