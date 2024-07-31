102ccdf428b6f0138726c76ecea1dfc340793619
anes05 <anesmsaddak5@gmail.com>
2024-07-31 15:19:33 +0100

    listening to webSocket Messaged done and working perfectly and deserialization of json is working

--- lib/utils/webSocket.dart
+++ lib/utils/webSocket.dart
@@ -3,7 +3,6 @@ import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:hex/hex.dart';
 void webSocketListen({required String sUrl, required String token, required String receiver,
 }) {
-  late Questions question;
   final wsUrl = "$sUrl?token=$token&store=$receiver";
   late WebSocketChannel channel ;
   try {
@@ -16,23 +15,31 @@ void webSocketListen({required String sUrl, required String token, required Stri
   channel.stream.listen(
         (message) {
       try {
-        print("Received message: $message");
+        String stringMessage= message.toString();
+/*
+        stringMessage=json.decode(stringMessage);
+*/
+        print("stringMessage: $stringMessage");
+        //print("Received message: $message");
         if (message is List<int>) {
           final decodedMessage = utf8.decode(message);
           print("Decoded message: $decodedMessage");
           // Decode the JSON string into a Map
           Map<String, dynamic> data = json.decode(decodedMessage);
-          String store = data['store'];
-          String id = data['question']['id'];
+          print(data);
+
+          //String store = data['store'];
+          //int id = data['question']['id'];
           String question = data['question']['question'];
           List<Map<String, dynamic>> options = List<Map<String, dynamic>>.from(data['question']['options']);
           for (var option in options) {
             String choice = option['choice'];
+            print(choice);
           }
           print("----------------------------------------------------------------------------------");
           print("question: $question");
-          print("id: $id");
-          print("choices: $options");
+          //print("id: $id");
+         // print("choices: $options");
         } else {
           print("Message is not a byte array");
         }
